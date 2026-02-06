ce32d5c679a7f55b4016272ec68e88ccd3dc708d
Jonathan Rugama <jrugama96@gmail.com>
2026-02-05 22:49:03 -0600

    Initiating project

---
+++ src/main/java/jrugama/laboratoriodos/LaboratorioDosApplication.java
@@ -0,0 +1,13 @@
+package jrugama.laboratoriodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioDosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorioDosApplication.class, args);
+    }
+
+}
